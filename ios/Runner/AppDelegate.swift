043c728f676e8d6cedb2f09c5c4677bced53c63b
lema <lema@lemas-Mac-mini.local>
2023-09-28 17:25:40 -0500

    ROLLBACK
    Roll back to only bff flow tracking

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // TODO: api-key-google
+    // <GMSServices.provideAPIKey("AIzaSyCDvAaF2cW8CAfWu-Cen70Q02p06q4VIbs")
+    GMSServices.provideAPIKey("AIzaSyCDvAaF2cW8CAfWu-Cen7oQ02po6q4VIbs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
